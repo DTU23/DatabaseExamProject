b92fd412a2010380c3f83f464bf55b1598b8ea01
Viktor Poulsen <viktormaigaard@gmail.com>
2017-04-04 14:46:01 +0200

    added implementation

--- src/main/java/dk/dtu_23/model/data/dao/MySQLProductBatchCompDAO.java
+++ src/main/java/dk/dtu_23/model/data/dao/MySQLProductBatchCompDAO.java
@@ -15,18 +15,24 @@ public class MySQLProductBatchCompDAO implements ProductBatchCompDAO {
 	private List<ProductBatchCompDTO> list = new ArrayList<ProductBatchCompDTO>();
 	@Override
 	public ProductBatchCompDTO getProductBatchComp(int pbId, int rbId) throws DALException {
-		return null;
+		ResultSet rs = Connector.doQuery("CALL get_batch_details_from_id = " + pbId);
+		try {
+			if (!rs.first()) throw new DALException("Produkt parti med " + pbId + " findes ikke");
+			return new ProductBatchCompDTO (rs.getInt("pbId"), rs.getInt("rbId"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("oprId"));
+		}
+		catch (SQLException e) {throw new DALException(e); }
+
 	}
 
 	@Override
 	public List<ProductBatchCompDTO> getProductBatchCompList(int pbId) throws DALException {
+		List<ProductBatchCompDTO> list = new ArrayList<ProductBatchCompDTO>();
 		ResultSet rs = Connector.doQuery("CALL get_batch_details_from_id("+pbId+");");
 		try
 		{
 			while (rs.next())
 			{
-				list.add(new ProductBatchCompDTO(
-				));
+				list.add(new ProductBatchCompDTO(rs.getInt("pbId"), rs.getInt("rbId"), rs.getDouble("tara"), rs.getDouble("netto"), rs.getInt("oprId")));
 			}
 		}
 		catch (SQLException e) { throw new DALException(e); }
@@ -34,18 +40,20 @@ public class MySQLProductBatchCompDAO implements ProductBatchCompDAO {
 	}
 
 	@Override
-	public List<ProductBatchCompDTO> getProductBatchCompList() throws DALException {
-		return this.list;
-	}
-
-	@Override
 	public void createProductBatchComp(ProductBatchCompDTO productbatchcomponent) throws DALException {
-		list.add(productbatchcomponent);
+		Connector.doUpdate("CALL create_product_batch_from_recipe_id(" + productbatchcomponent.getPbId() + "," + productbatchcomponent.getRbId() + "," + productbatchcomponent.getTara() + "," +
+				productbatchcomponent.getNetto() + "," + productbatchcomponent.getOprId() + ");"); // rigtig CALL?
 	}
 
 	@Override
 	public void updateProductBatchComp(ProductBatchCompDTO productbatchcomponent) throws DALException {
-		list.remove(productbatchcomponent);
-		list.add(productbatchcomponent);
+		Connector.doUpdate("CALL update_product_batch(" + productbatchcomponent.getPbId() + "," + productbatchcomponent.getRbId() + "," + productbatchcomponent.getTara() + "," +
+				productbatchcomponent.getNetto() + "," + productbatchcomponent.getOprId() + ");"); // rigtig CALL?
+	}
+
+	@Override
+	public List<ProductBatchCompDTO> getProductBatchCompList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
