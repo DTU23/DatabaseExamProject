ef9a0f8e8416a7006a6c09b5c23fdca702556662
Viktor Poulsen <viktormaigaard@gmail.com>
2017-04-04 12:17:37 +0200

    added test

---
+++ src/test/java/dk/dtu_23/model/data/dao/MySQLProduceBatchDAOTest.java
@@ -0,0 +1,38 @@
+package dk.dtu_23.model.data.dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MySQLProduceBatchDAOTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getProduceBatch() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void getProduceBatchList() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void createProduceBatch() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void updateProduceBatch() throws Exception {
+        assertEquals(1,1);
+    }
+
+}
