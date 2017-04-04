cb298f0a495e95f004cf6982ed79b695c06b6e87
Viktor Poulsen <viktormaigaard@gmail.com>
2017-04-04 19:23:51 +0200

    Merge branch 'feature/verify_code' into develop
    Optimzed variables

--- src/main/java/dk/dtu_23/control/api/v1/Operator.java
+++ src/main/java/dk/dtu_23/control/api/v1/Operator.java
@@ -12,27 +12,29 @@ import java.util.List;
 
 @Path("api/v1/operator")
 public class Operator {
-    private List<OperatorDTO> list = new ArrayList<>();
     private MySQLOperatorDAO DAO = new MySQLOperatorDAO();
-    public Operator()
-    {
-        try {
-            list.addAll(this.DAO.getOperatorList());
-        } catch (DALException e){
-            e.printStackTrace();
-        }
-    }
 
     @GET
     @Path("{opr_id}")
     @Produces(MediaType.APPLICATION_JSON)
     public OperatorDTO getOperator(@PathParam("opr_id") String oprId) {
-        return list.get(Integer.parseInt(oprId));
+        try {
+            return this.DAO.getOperator(Integer.parseInt(oprId));
+        } catch (DALException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<OperatorDTO> getOperator() {
+        List<OperatorDTO> list = new ArrayList<>();
+        try {
+            list.addAll(this.DAO.getOperatorList());
+        } catch (DALException e){
+            e.printStackTrace();
+        }
         return list;
     }
 }
