93306cb974ebf94044ca00224b9fea45f7983ca4
Frederik Værnegaard <frederik@nyka.dk>
2017-04-18 09:06:49 +0200

    changed exception handling in tests

--- src/test/java/dk/dtu_23/model/data/dao/MySQLProduceBatchDAOTest.java
+++ src/test/java/dk/dtu_23/model/data/dao/MySQLProduceBatchDAOTest.java
@@ -1,13 +1,11 @@
 package dk.dtu_23.model.data.dao;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.After;
@@ -20,163 +18,140 @@ import dk.dtu_23.model.data.connector.Connector;
 import dk.dtu_23.model.data.interfaces.DALException;
 
 public class MySQLProduceBatchDAOTest {
-	
+
 	MySQLProduceBatchDAO produceBatch;
-	
-    @Before
-    public void setUp() throws Exception {
-    	try { new Connector(); } 
-		catch (InstantiationException e) { e.printStackTrace(); }
-		catch (IllegalAccessException e) { e.printStackTrace(); }
-		catch (ClassNotFoundException e) { e.printStackTrace(); }
-		catch (SQLException e) { e.printStackTrace(); }
-		
+
+	@Before
+	public void setUp() throws Exception {
+		new Connector();		
 		produceBatch = new MySQLProduceBatchDAO();
-    }
+	}
 
-    @After
-    public void tearDown() {
-    	produceBatch = null;
-    }
+	@After
+	public void tearDown() throws Exception{
+		produceBatch = null;
+	}
 
-    /**
-     * Positive test. Get any produce batch by ID.
-     */
-    @Test
-    public void testGetProduceBatchByID() {
-    	ProduceBatchDTO expected = new ProduceBatchDTO(1, "dej", "Wawelka", 1000);
-    	ProduceBatchDTO actual = null;
-    	
+	/**
+	 * Positive test. Get any produce batch by ID.
+	 */
+	@Test
+	public void testGetProduceBatchByID() throws Exception {
+		ProduceBatchDTO expected = new ProduceBatchDTO(1, "dej", "Wawelka", 1000);
+		ProduceBatchDTO actual = null;
+
 		try {
 			actual = produceBatch.getProduceBatch(1);
 		} catch (DALException e) {
 			System.out.println(e.getMessage());
 		}
-    	
-        assertThat(actual, is(equalTo(expected)));
-    }
-    
-    /**
-     * Negative test. Try to get produce batch by ID that doesn't exist.
-     */
-    @Test
-    public void testGetProduceBatchByIDThatDoesntExist() {
-    	ProduceBatchDTO actual = null;
-    	String output = null;
-    	
+
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * Negative test. Try to get produce batch by ID that doesn't exist.
+	 */
+	@Test
+	public void testGetProduceBatchByIDThatDoesntExist() throws Exception{
+		ProduceBatchDTO actual = null;
+		String output = null;
+
 		try {
 			actual = produceBatch.getProduceBatch(17);
 		} catch (DALException e) {
+			e.printStackTrace();
 			output = e.getMessage();
 			System.out.println(output);
 		}
-    	
+
 		assertThat(output, notNullValue());
-		assertThat(output, is(equalTo("Produce batch with id 17 does not exist")));
-        assertThat(actual, nullValue());
-    }
+		assertThat(output, is("Produce batch with id 17 does not exist"));
+		assertThat(actual, nullValue());
+	}
 
-    /**
-     * Positive test. Get the produce batch list.
-     */
-    @Test
-    public void testGetProduceBatchList() {
-       List<ProduceBatchDTO> pbList = null;
-       // Equal to the element in second row of the view produce_batch_list
-       ProduceBatchDTO produceBatchDTO = new ProduceBatchDTO(2, "tomat", "Knoor", 300);
-       
-       try {
-    	   pbList = produceBatch.getProduceBatchList();
-       } catch(DALException e) {
-    	   System.out.println(e.getMessage());
-       }
-       
-       // Print out the produce batch list to console
-       for(int i = 0; i < pbList.size(); i++)
-    	   System.out.println(pbList.get(i));
-       
-       assertThat(pbList, notNullValue());
-       assertThat(pbList.get(1), is(equalTo(produceBatchDTO)));
-    }
+	/**
+	 * Positive test. Get the produce batch list.
+	 */
+	@Test
+	public void testGetProduceBatchList() throws Exception{
+		List<ProduceBatchDTO> pbList = null;
+		// Equal to the element in second row of the view produce_batch_list
+		ProduceBatchDTO produceBatchDTO = new ProduceBatchDTO(2, "tomat", "Knoor", 300);
 
-    /**
-     * Positive test. Create a produce batch.
-     */
-    @Test
-    public void testCreateProduceBatch() {
-        double amount = 500;
-        ProduceDTO produceDTO = new ProduceDTO(8, "chips", "Kims");
-        ProduceBatchDTO expected = new ProduceBatchDTO(8, "chips", "Kims", amount);
-        ProduceBatchDTO actual = null;
-        
-        try {
-        	produceBatch.createProduceBatch(produceDTO, amount);
-        	actual = produceBatch.getProduceBatch(8);
-        } catch (DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(equalTo(expected)));
-    }
-    
-    /**
-     * Negative test. Create a produce batch with already existing ID.
-     */
-    @Test
-    public void testCreateProduceBatchWithAlreadyExistingID() {
-        double amount = 500;
-        ProduceDTO produceDTO = new ProduceDTO(5, "chips", "Kims");
-        ProduceBatchDTO expected = new ProduceBatchDTO(5, "chips", "Kims", amount);
-        ProduceBatchDTO actual = null;
-        
-        try {
-        	produceBatch.createProduceBatch(produceDTO, amount);
-        	actual = produceBatch.getProduceBatch(5);
-        } catch (DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(not(equalTo(expected))));
-//        assertThat(actual.getProduceName(), is(not(equalTo("chips"))));
-    }
+		pbList = produceBatch.getProduceBatchList();
 
-    /**
-     * Positive test. Update a produce batch.
-     */
-    @Test
-    public void testUpdateProduceBatch() {
-        ProduceBatchDTO pbDTO = new ProduceBatchDTO(1, "dej", "Wawelka", 1000);
-        ProduceBatchDTO actual = null;
-        double amount = 800;
-        
-        try {
-        	produceBatch.updateProduceBatch(pbDTO, amount);
-        	actual = produceBatch.getProduceBatch(1);
-        } catch(DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(not(equalTo(pbDTO))));
-        assertThat(actual.getAmount(), is(equalTo(amount)));
-    }
-    
-    /**
-     * Negative test. Update a produce batch that doesn't exist.
-     */
-    @Test
-    public void testUpdateProduceBatchThatDoesntExist() {
-        ProduceBatchDTO pbDTO = new ProduceBatchDTO(12, "ketchup", "Heinz", 200);
-        ProduceBatchDTO actual = null;
-        double amount = 400;
-        
-        try {
-        	produceBatch.updateProduceBatch(pbDTO, amount);
-        	actual = produceBatch.getProduceBatch(12);
-        } catch(DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(not(equalTo(pbDTO))));
-    }
+		// Print out the produce batch list to console
+		for(int i = 0; i < pbList.size(); i++)
+			System.out.println(pbList.get(i));
+
+		assertThat(pbList, notNullValue());
+		assertThat(pbList.get(1), is(produceBatchDTO));
+	}
+
+	/**
+	 * Positive test. Create a produce batch.
+	 */
+	@Test
+	public void testCreateProduceBatch() throws Exception{
+		double amount = 500;
+		ProduceDTO produceDTO = new ProduceDTO(8, "chips", "Kims");
+		ProduceBatchDTO expected = new ProduceBatchDTO(8, "chips", "Kims", amount);
+		ProduceBatchDTO actual = null;
+
+		produceBatch.createProduceBatch(produceDTO, amount);
+		actual = produceBatch.getProduceBatch(8);
+
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * Negative test. Create a produce batch with already existing ID.
+	 */
+	@Test
+	public void testCreateProduceBatchWithAlreadyExistingID() throws Exception {
+		double amount = 500;
+		ProduceDTO produceDTO = new ProduceDTO(5, "chips", "Kims");
+		ProduceBatchDTO expected = new ProduceBatchDTO(5, "chips", "Kims", amount);
+		ProduceBatchDTO actual = null;
+
+		produceBatch.createProduceBatch(produceDTO, amount);
+		actual = produceBatch.getProduceBatch(5);
+
+		assertThat(actual, is(not(expected)));
+		//        assertThat(actual.getProduceName(), is(not("chips")));
+	}
+
+	/**
+	 * Positive test. Update a produce batch.
+	 */
+	@Test
+	public void testUpdateProduceBatch() throws Exception {
+		ProduceBatchDTO pbDTO = new ProduceBatchDTO(1, "dej", "Wawelka", 1000);
+		ProduceBatchDTO actual = null;
+		double amount = 800;
+
+		produceBatch.updateProduceBatch(pbDTO, amount);
+		actual = produceBatch.getProduceBatch(1);
+
+		assertThat(actual, is(not(pbDTO)));
+		assertThat(actual.getAmount(), is(amount));
+	}
+
+	/**
+	 * Negative test. Update a produce batch that doesn't exist.
+	 */
+	@Test
+	public void testUpdateProduceBatchThatDoesntExist() throws Exception{
+		ProduceBatchDTO pbDTO = new ProduceBatchDTO(12, "ketchup", "Heinz", 200);
+		ProduceBatchDTO actual = null;
+		double amount = 400;
+
+
+		produceBatch.updateProduceBatch(pbDTO, amount);
+		actual = produceBatch.getProduceBatch(12);
+
+		assertThat(actual, is(not(pbDTO)));
+	}
 
 }
--- src/test/java/dk/dtu_23/model/data/dao/MySQLProduceDAOTest.java
+++ src/test/java/dk/dtu_23/model/data/dao/MySQLProduceDAOTest.java
@@ -1,9 +1,11 @@
 package dk.dtu_23.model.data.dao;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.After;
@@ -13,162 +15,128 @@ import org.junit.Test;
 import dk.dtu_23.model.ProduceDTO;
 import dk.dtu_23.model.ProduceOverviewDTO;
 import dk.dtu_23.model.data.connector.Connector;
-import dk.dtu_23.model.data.interfaces.DALException;
 
 public class MySQLProduceDAOTest {
-	
+
 	MySQLProduceDAO produce;
-	
-    @Before
-    public void setUp() throws Exception {
-		try { new Connector(); } 
-		catch (InstantiationException e) { e.printStackTrace(); }
-		catch (IllegalAccessException e) { e.printStackTrace(); }
-		catch (ClassNotFoundException e) { e.printStackTrace(); }
-		catch (SQLException e) { e.printStackTrace(); }
+
+	@Before
+	public void setUp() throws Exception {
+		new Connector();
 		produce = new MySQLProduceDAO();
-    }
+	}
 
-    @After
-    public void tearDown() {
-    	produce = null;
-    }
-    
-    /**
-     * Positive test. Get any produce by ID.
-     */
-    @Test
-    public void testGetProduceByID() {
-    	ProduceDTO actual = null;
+	@After
+	public void tearDown() throws Exception{
+		produce = null;
+	}
+
+	/**
+	 * Positive test. Get any produce by ID.
+	 */
+	@Test
+	public void testGetProduceByID() throws Exception {
+		ProduceDTO actual = null;
 		ProduceDTO expected = new ProduceDTO(2, "tomat", "Knoor");
-		
-		try {
-			actual = produce.getProduce(2);
-		} catch (DALException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		assertThat(actual, is(equalTo(expected)));
-    }
 
-    /**
-     * Negative test. Try to get produce with non-existing ID.
-     */
-    @Test
-    public void testGetProduceByIDThatDoesntExist() {
-    	String output = null;
-    	ProduceDTO actual = null;
-    	
-		try {
-			actual = produce.getProduce(10);
-		} catch (DALException e) {
-			output = e.getMessage();
-			System.out.println(output);
-		}
-		
+		actual = produce.getProduce(2);
+
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * Negative test. Try to get produce with non-existing ID.
+	 */
+	@Test
+	public void testGetProduceByIDThatDoesntExist() throws Exception {
+		String output = null;
+		ProduceDTO actual = null;
+
+		actual = produce.getProduce(10);
+
 		assertThat(output, notNullValue());
-		assertThat(output, is(equalTo("Produce with id 10 does not exist")));
+		assertThat(output, is("Produce with id 10 does not exist"));
 		assertThat(actual, nullValue());
-    }
+	}
 
-    /**
-     * Positive test. Get all produces from the produce list.
-     */
-    @Test
-    public void testGetProduceList() {
-    	List<ProduceDTO> produceList = null;
-    	// Last row in our table, produce
-    	ProduceDTO produceDTO = new ProduceDTO(7, "champignon", "Igloo Frostvarer");
-    	
-    	try {
-    		produceList = produce.getProduceList();
-    	} catch(DALException e) {
-    		System.out.println(e.getMessage());
-    	}
-    	
-    	// Print out the produce list to console
-    	for(int i = 0; i < produceList.size(); i++)
-    		System.out.println(produceList.get(i));
-    	
-    	assertThat(produceList, notNullValue());
-    	assertThat(produceList.get(6), is(equalTo(produceDTO)));
-    }
-    
-    /**
-     * Positive test. Create a produce.
-     */
-    @Test
-    public void testCreateProduce() {
-        ProduceDTO expected = new ProduceDTO(8, "smør", "Kærgården");
-        ProduceDTO actual = null;
-        
-        try {
-        	produce.createProduce(expected);
-        	actual = produce.getProduce(8);
-        } catch (DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(equalTo(expected)));
-    }
-    
-    /**
-     * Negative test. Try to overwrite already created produce
-     */
-    @Test
-    public void testCreateProduceWithAlreadyUsedID() {
-        ProduceDTO newProduce = new ProduceDTO(1, "vand", "Aqua");
-        ProduceDTO actual = null;
-        
-        try {
-        	produce.createProduce(newProduce);
-        	actual = produce.getProduce(1);
-        } catch (DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(not(equalTo(newProduce))));
-    }
+	/**
+	 * Positive test. Get all produces from the produce list.
+	 */
+	@Test
+	public void testGetProduceList() throws Exception {
+		List<ProduceDTO> produceList = null;
+		// Last row in our table, produce
+		ProduceDTO produceDTO = new ProduceDTO(7, "champignon", "Igloo Frostvarer");
 
-    /**
-     * Positive test. Update produce
-     */
-    @Test
-    public void testUpdateProduce() {
-    	ProduceDTO expected = new ProduceDTO(4, "tomat", "Kims");
-        ProduceDTO actual = null;
-        
-        try {
-        	produce.updateProduce(expected);
-        	actual = produce.getProduce(4);
-        } catch (DALException e) {
-        	System.out.println(e.getMessage());
-        }
-        
-        assertThat(actual, is(equalTo(expected)));
-    }
-    
-    /**
-     * Positive test. Get produce overview
-     */
-    @Test
-    public void testGetProduceOverview() {
-    	List<ProduceOverviewDTO> produceOverview = null;
-    	// First row in our view, produce_overview
-    	ProduceOverviewDTO produceOverviewDTO = new ProduceOverviewDTO(7, "champignon", 100);
-    	
-    	try {
-    		produceOverview = produce.getProduceOverview();
-    	} catch(DALException e) {
-    		System.out.println(e.getMessage());
-    	}
-    	
-    	// Print out the produce overview to console
-    	for(int i = 0; i < produceOverview.size(); i++)
-    		System.out.println(produceOverview.get(i));
-    	
-    	assertThat(produceOverview, notNullValue());
-    	assertThat(produceOverview.get(0), is(equalTo(produceOverviewDTO)));
-    }
+		produceList = produce.getProduceList();
+
+		// Print out the produce list to console
+		for(int i = 0; i < produceList.size(); i++)
+			System.out.println(produceList.get(i));
+
+		assertThat(produceList, notNullValue());
+		assertThat(produceList.get(6), is(produceDTO));
+	}
+
+	/**
+	 * Positive test. Create a produce.
+	 */
+	@Test
+	public void testCreateProduce() throws Exception {
+		ProduceDTO expected = new ProduceDTO(8, "smør", "Kærgården");
+		ProduceDTO actual = null;
+
+		produce.createProduce(expected);
+		actual = produce.getProduce(8);
+
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * Negative test. Try to overwrite already created produce
+	 */
+	@Test
+	public void testCreateProduceWithAlreadyUsedID() throws Exception {
+		ProduceDTO newProduce = new ProduceDTO(1, "vand", "Aqua");
+		ProduceDTO actual = null;
+
+		produce.createProduce(newProduce);
+		actual = produce.getProduce(1);
+
+		assertThat(actual, is(not(newProduce)));
+	}
+
+	/**
+	 * Positive test. Update produce
+	 */
+	@Test
+	public void testUpdateProduce() throws Exception {
+		ProduceDTO expected = new ProduceDTO(4, "tomat", "Kims");
+		ProduceDTO actual = null;
+
+		produce.updateProduce(expected);
+		actual = produce.getProduce(4);
+
+		assertThat(actual, is(expected));
+	}
+
+	/**
+	 * Positive test. Get produce overview
+	 */
+	@Test
+	public void testGetProduceOverview() throws Exception {
+		List<ProduceOverviewDTO> produceOverview = null;
+		// First row in our view, produce_overview
+		ProduceOverviewDTO produceOverviewDTO = new ProduceOverviewDTO(7, "champignon", 100);
+
+		produceOverview = produce.getProduceOverview();
+
+		// Print out the produce overview to console
+		for(int i = 0; i < produceOverview.size(); i++)
+			System.out.println(produceOverview.get(i));
+
+		assertThat(produceOverview, notNullValue());
+		assertThat(produceOverview.get(0), is(produceOverviewDTO));
+	}
 
 }
